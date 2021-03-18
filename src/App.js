03a0872b032621b336cc287ac1dd38863b84705d
mdehsanul <mdehsanulhaqueshanto04@gmail.com>
2021-03-18 08:44:08 +0600

    8-core-concept-updated

--- src/App.js
+++ src/App.js
@@ -1,61 +1,57 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 // main Component
 function App() {
   // random user generate from API
   const [nayoks, setNayoks] = useState([]);
-  useEffect(() =>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => setNayoks(data))
-  }, [])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setNayoks(data));
+  }, []);
   return (
     <div className="App">
       <MovieCounter></MovieCounter>
       {/* custom element -> custom element not same as HTML elements*/}
       {/* accessing random user  from API */}
-      {
-        nayoks.map(nk => <Nayok name={nk.name} key={nk.id} email={nk.email}></Nayok>)
-      }
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      {nayoks.map((nk) => (
+        <Nayok name={nk.name} key={nk.id} email={nk.email}></Nayok>
+      ))}
     </div>
   );
 }
 
 // new component
-function MovieCounter(){
+function MovieCounter() {
   let [count, setCount] = useState(0);
   const handleClick = () => setCount(count + 1);
-  return(
+  return (
     <div>
       <button onClick={handleClick}>Add Movies</button>
       <h3>Number of Movies: {count}</h3>
-      <MovieDisplay movies={count}></ MovieDisplay>
+      <MovieDisplay movies={count}></MovieDisplay>
     </div>
-  )
+  );
 }
 
 // new component
-function MovieDisplay(props){
-  return(
-    <h4>Movies I have acted: {props.movies}</h4>
-  )
+function MovieDisplay(props) {
+  return <h4>Movies I have acted: {props.movies}</h4>;
 }
 
 // new component
-function Nayok(props){
+function Nayok(props) {
   const nayokStyle = {
     border: "3px solid purple",
-    margin: "20px"
-  }
-  return <div style={nayokStyle}>
-    <h1>Name: {props.name}</h1>
-    <p>Email: {props.email}</p>
-  </div>
+    margin: "20px",
+  };
+  return (
+    <div style={nayokStyle}>
+      <h1>Name: {props.name}</h1>
+      <p>Email: {props.email}</p>
+    </div>
+  );
 }
 
 export default App;
